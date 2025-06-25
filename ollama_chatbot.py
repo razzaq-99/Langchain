26cbfe7e3243e4e417cc476fbcc7f5f124fe68f1
abdul Razzaq <ar8937562@gmail.com>
2025-06-25 14:58:29 +0500

    Ollama chatbot by using Langchain

---
+++ ollama_chatbot.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_community.llms import ollama
+
+
+llm = ollama.Ollama(
+    model="llama2"  )
+
+
+st.set_page_config(page_title="Abdul Chatbot")
+st.title("💬 Abdul Chatbot")
+# st.markdown("Ask anything!")
+
+
+prompt = PromptTemplate.from_template("Q: {question} \nA:")
+
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+
+user_input = st.text_input("Ask a question:")
+
+
+if user_input:
+    response = chain.run(question=user_input)
+    st.text(response)
+    st.write("🤖 Abdul")
+    st.write("Answer:", response)
+
+st.write("Powered by Ollama and LangChain")
+st.write("Made with ❤️ by Abdul Razzaq")
