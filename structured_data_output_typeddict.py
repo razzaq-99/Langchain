0e0415e07ae2537504377f2a325e7a641dfe2e80
abdul Razzaq <ar8937562@gmail.com>
2025-07-22 18:11:26 +0500

    Annotated property in Typedict for strutured extraction of data

--- structured_data_output_typeddict.py
+++ structured_data_output_typeddict.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict,Annotated,Optional
 from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 import os
@@ -10,21 +10,32 @@ os.environ["LANGCHAIN_API_KEY"] = ""
 model = ChatOllama(model='gemma:2b')
 
 class Reviews(TypedDict):
-    review: str
-    sentiment: str
+    key_theme: Annotated[list[str],"Write down all the themes mentioned in the review"]
+    review: Annotated[str,"description of the user review"]
+    sentiment: Annotated[str,"Return user review/sentiment type"]
+    reviewer_name: Annotated[str,"Name of the reviewer"]
+    pros: Annotated[Optional[list[str]],"Write down all the pros mentioned in the review"]
+    cons: Annotated[Optional[list[str]],"Write down all the cons mentioned in the review"]
     
 structured_model = model.with_structured_output(Reviews)
 
-result1 = structured_model.invoke("Overall, I wouldn't say the products are bad — there’s a lot of potential here, especially in terms of fabric and design — but some areas like quality control and sizing need improvement. I'd consider ordering again, but probably only after checking more customer reviews next time.")
+result1 = structured_model.invoke("""Overall, I wouldn't say the products are bad — there’s a lot of potential here, especially in terms of fabric and design — but some areas like quality control and sizing need improvement. I'd consider ordering again, but probably only after checking more customer reviews next time. 
+                                  
+    Review by Ahad Channa""")
 
-result2 = structured_model.invoke("I'm not sure if I can recommend this product to anyone. The quality is poor, and the sizing is off. I wouldn't buy it again.")
 
-result3 = structured_model.invoke("The product is very great and cost-effective,highly recommended")
+# result2 = structured_model.invoke("I'm not sure if I can recommend this product to anyone. The quality is poor, and the sizing is off. I wouldn't buy it again.")
 
-print(result1)
-print(result2)
-print(result3)
+# result3 = structured_model.invoke("The product is very great and cost-effective,highly recommended")
 
-print(result1["sentiment"])
-print(result2["sentiment"])
-print(result3["sentiment"])
+# print(result1)
+# print(result2)
+# print(result3)
+
+# print(result1["sentiment"])
+# print(result2["sentiment"])
+# print(result3["sentiment"])
+
+print(result1['reviewer_name'])
+print("Pros : ",result1['pros'])
+print("Cons : ",result1['cons'])
